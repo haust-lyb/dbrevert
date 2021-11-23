41f754104d7fb44bc0ca43983fa9a6e6e2a27d33
haust_lyb <ace1570194845@gmail.com>
2021-11-23 23:22:29 +0800

    改良

--- backupandrevert.py
+++ backupandrevert.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python
+# _*_ coding:UTF-8 _*_
 import os
+import platform
 
+
+# 重要参数配置
 db_name = "ylt"
 db_user_name = "root"
 db_user_password = "rootroot"
-work_space = os.getcwd()+"/history"
 
+
+if platform.system() == 'Windows':
+    os.system('chcp 65001')
+
+work_space = os.getcwd()+os.sep+"history"
 if not os.path.exists(work_space):
     os.makedirs(work_space)
 
@@ -15,35 +24,40 @@ print("### 欢迎使用数据库备份恢复系统1.0 ###\n"
       "------工作的空间：%s\n"
       %(db_name,db_user_name,db_user_password,work_space))
 
-def work_space_file_list():
-    print("### 当前工作空间下的备份文件### ")
-    print(os.popen("ls -alh %s"%(work_space)).read())
-
-work_space_file_list()
 command = input("请输入要执行的操作：\n"
       "1:备份 2:恢复 其他:退出（exit)\n"
                 "请输入：")
 def backup():
     backfilename = input("输入备份文件名字：")
-    cmd = "mysqldump -u%s -h127.0.0.1 -p%s %s --single-transaction > %s/backup_%s.sql"%(db_user_name,db_user_password,db_name,work_space,backfilename)
+    targetFilePath = "%s%sbackup_%s.sql"%(work_space,os.sep,backfilename);
+    cmd = "mysqldump -u%s -h127.0.0.1 -p%s %s --single-transaction > %s"%(db_user_name,db_user_password,db_name,targetFilePath)
     print(cmd)
     result = os.popen(cmd).read()
     print(result)
+    if os.path.getsize(targetFilePath) == 0:
+        print("【 x 备份失败 x 】")
+    else:
+        print("【 √ 备份完成 √ 】")
+
 
 def revert():
     print("备份文件名字只需输入backup_之后的名字即可，且无需包含后缀.sql")
     backfilename = input("输入备份文件名字：")
-    cmd = "mysql -u%s -h127.0.0.1 -p%s %s < %s/backup_%s.sql"%(db_user_name,db_user_password,db_name,work_space,backfilename)
+    targetFilePath = "%s%sbackup_%s.sql"%(work_space,os.sep,backfilename);
+    cmd = "mysql -u%s -h127.0.0.1 -p%s %s < %s"%(db_user_name,db_user_password,db_name,targetFilePath)
     print(cmd)
     result = os.popen(cmd).read()
     print(result)
+
 if command == "1":
-    # print("备份")
+    print("【启动备份】...")
+    print("【备份中】，请等待程序执行完毕...")
     backup()
-    work_space_file_list()
 elif command == "2":
-    # print("恢复")
+    print("开始恢复...")
+    print("恢复中，请等待程序执行完毕...")
     revert()
+    print(" √ 恢复执行完毕 √ ")
 else:
     print("未知命令，程序退出")
 
